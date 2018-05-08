37dba0d6fcc162e6a961a214a65bb25a0f856e60
abmomin <abdulmomen130@gmail.com>
2018-05-08 12:18:52 +0600

    initial commit

---
+++ src/main/java/com/bookpublisher/bookpublisher/BookpublisherApplication.java
@@ -0,0 +1,12 @@
+package com.bookpublisher.bookpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookpublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookpublisherApplication.class, args);
+	}
+}
