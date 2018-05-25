5661a40f24e6453bee934ad1111dc761c1a368cc
Mamun <mamun0589@gmail.com>
2018-05-25 21:48:51 +0600

    save book image information to database and image to uploads folder. Set configuration for showing image in front end from uploads folder

--- src/main/java/com/bookpublisher/bookpublisher/controllers/PublishBookController.java
+++ src/main/java/com/bookpublisher/bookpublisher/controllers/PublishBookController.java
@@ -2,6 +2,7 @@ package com.bookpublisher.bookpublisher.controllers;
 
 import com.bookpublisher.bookpublisher.models.Author;
 import com.bookpublisher.bookpublisher.models.Book;
+import com.bookpublisher.bookpublisher.models.BookImage;
 import com.bookpublisher.bookpublisher.models.BookReview;
 import com.bookpublisher.bookpublisher.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +13,15 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,25 +30,45 @@ import java.util.List;
  */
 @Controller
 public class PublishBookController {
+    private static String UPLOADED_FOLDER = "uploads/";
     @Autowired
     BookRepository bookRepository;
     Book tempBook;
-
-    @RequestMapping(value = "/saveBook", method = RequestMethod.POST)
-    public ModelAndView showSaveBookPage(@ModelAttribute Book book, @ModelAttribute("numberOfAuthor") int rows) {
-        tempBook = book;
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("addAuthors");
-        modelAndView.addObject("rows",rows-1);
-        modelAndView.addObject("book", new Book());
-        return modelAndView;
-    }
 
     @RequestMapping(value = "/saveBook")
     public ModelAndView showSaveNewBookPage(@ModelAttribute Book book) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("addBook");
         modelAndView.addObject("book", book);
+        modelAndView.addObject("book", new Book());
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/saveBook", method = RequestMethod.POST)
+    public ModelAndView showSaveBookPage(@ModelAttribute Book book, @ModelAttribute("numberOfAuthor") int rows,
+                                         @RequestParam("imageFile") MultipartFile imageFile,
+                                         @RequestParam("previewImageFile") MultipartFile previewImageFile) {
+
+        book.setBookImage(new BookImage(book.getIsbn()+imageFile.getOriginalFilename(), book.getIsbn()+previewImageFile.getOriginalFilename()));
+        tempBook = book;
+
+        try {
+            // Get the file and save it somewhere
+            byte[] bytes = imageFile.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + book.getIsbn() + imageFile.getOriginalFilename());
+            Files.write(path, bytes);
+
+            bytes = previewImageFile.getBytes();
+            Path path2 = Paths.get(UPLOADED_FOLDER + book.getIsbn() + previewImageFile.getOriginalFilename());
+            Files.write(path2, bytes);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("addAuthors");
+        modelAndView.addObject("rows",rows-1);
         modelAndView.addObject("book", new Book());
         return modelAndView;
     }
--- src/main/java/com/bookpublisher/bookpublisher/models/Author.java
+++ src/main/java/com/bookpublisher/bookpublisher/models/Author.java
@@ -10,16 +10,14 @@ public class Author {
     private long id;
     private String name;
     private String email;
-    private String authorImage;
     private String info;
 
     public Author() {
     }
 
-    public Author(String name, String email, String authorImage, String info) {
+    public Author(String name, String email, String info) {
         this.name = name;
         this.email = email;
-        this.authorImage = authorImage;
         this.info = info;
     }
 
@@ -55,21 +53,12 @@ public class Author {
         this.info = info;
     }
 
-    public String getAuthorImage() {
-        return authorImage;
-    }
-
-    public void setAuthorImage(String authorImage) {
-        this.authorImage = authorImage;
-    }
-
     @Override
     public String toString() {
         return "Author{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
-                ", authorImage='" + authorImage + '\'' +
                 ", info='" + info + '\'' +
                 '}';
     }
